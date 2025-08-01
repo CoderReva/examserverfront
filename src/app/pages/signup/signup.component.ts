c641e6de13a1a42119c025282ac293265a50b1d5
Revaram bamne <revarambamne8@gmail.com>
2025-08-01 23:27:38 +0530

    formSubmit()
    formSubmit(), import OnnInit

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -11,5 +11,16 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+export class SignupComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic can go here
+  }
+
+  formSubmit() {
+    // Handle form submission logic here
+    alert('Form submitted!');
+  }
 
 }
