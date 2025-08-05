420191fdb3f9106f22a4f6d97a7032a334f54113
Revaram bamne <revarambamne8@gmail.com>
2025-08-05 11:52:38 +0530

    Remove mycomponents folder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
       provideClientHydration(withEventReplay()),
-      importProvidersFrom(MatButtonModule)
+      importProvidersFrom(MatButtonModule, FormsModule),
+      
     ]
 };
